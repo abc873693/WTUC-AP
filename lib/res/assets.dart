ab8c0974599a9ba4ddfbc3fdca364a7c3ddfe2aa
RainVisitor <abc873693@gmail.com>
2020-10-19 22:45:13 +0800

    Remove useless assets

--- lib/res/assets.dart
+++ lib/res/assets.dart
@@ -6,12 +6,4 @@ class ImageAssets {
 
 class FileAssets {
   static const String basePath = 'assets/';
-
-  static String courses = '$basePath/coursetable.json';
-  static String scores = '$basePath/scores.json';
-  static String semesters = '$basePath/semesters.json';
-
-  static String userInfo = '$basePath/user_info.json';
-
-  static String notifications = '$basePath/notifications.json';
 }
