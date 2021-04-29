11075af1a1f92633b4a4c8a970daf977e074f571
RainVisitor <abc873693@gmail.com>
2021-04-29 12:48:39 +0800

    Update change log source to asset file

--- lib/res/assets.dart
+++ lib/res/assets.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
 class ImageAssets {
   static const String basePath = 'assets/images';
 
@@ -7,4 +11,11 @@ class ImageAssets {
 
 class FileAssets {
   static const String basePath = 'assets/';
+
+  static const String changelog = 'changelog.json';
+  static const String carParkArea = '$basePath/car_park_area.json';
+
+  static Future<Map<String, dynamic>> get changelogData async {
+    return jsonDecode(await rootBundle.loadString(changelog));
+  }
 }
