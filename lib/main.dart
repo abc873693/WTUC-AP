f88e2fa2ccef8067571b62ae1ff6183a6efa224e
RainVisitor <abc873693@gmail.com>
2021-09-11 19:31:33 +0800

    Add desktop google sign in setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:ap_common/api/announcement_helper.dart';
 import 'package:ap_common/utils/preferences.dart';
@@ -7,6 +8,7 @@ import 'package:ap_common_firebase/utils/firebase_utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in_dartio/google_sign_in_dartio.dart';
 
 import 'app.dart';
 import 'config/constants.dart';
@@ -23,6 +25,11 @@ void main() async {
   if (FirebaseUtils.isSupportCore) await Firebase.initializeApp();
   AnnouncementHelper.instance.organization = 'wtuc';
   AnnouncementHelper.instance.appleBundleId = 'com.wtuc.ap';
+  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux))
+    GoogleSignInDart.register(
+      clientId:
+          '424476440071-1u6ogh95cl7a6tosoco42hum7q06nff2.apps.googleusercontent.com',
+    );
   if (!kDebugMode && FirebaseCrashlyticsUtils.isSupported) {
     await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
     runZonedGuarded(() {
